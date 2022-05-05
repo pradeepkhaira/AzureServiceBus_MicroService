5218be29003805ab5625ea4c17672966d8378dde
pradeep.singh <pradeep.singh@BLR1-LHP-N79310>
2022-05-05 18:10:15 +0530

    adding AzureProject

---
+++ BurgerKingOrder.MicroService/Controllers/BKOrderController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BurgerKingOrder.MicroService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BKOrderController : ControllerBase
+    {
+        // GET: api/<BKOrderController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<BKOrderController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BKOrderController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BKOrderController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BKOrderController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
